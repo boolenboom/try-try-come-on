ca400d77188cd226ab3ec8ff55b90f4e4e277d38
boolenboom <alan93072@gmail.com>
2022-01-06 18:34:52 +0800

    重構 fetcher工廠

--- subscribe-demo/js/APIFetcher.js
+++ subscribe-demo/js/APIFetcher.js
@@ -1,83 +1,137 @@
 ;
-function getAuthorizationHeader() {
-    //  填入自己 ID、KEY 開始
-    // let AppID = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF';
-    // let AppKey = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF';
-    let AppID = 'eb78cef36689451a8aab0db192c90462';
-    let AppKey = 'BcSgzENVt1N5FxsK0SOjFiAU3eA';
-    //  填入自己 ID、KEY 結束
-    let GMTString = new Date().toGMTString();
-    let ShaObj = new jsSHA('SHA-1', 'TEXT');
-    ShaObj.setHMACKey(AppKey, 'TEXT');
-    ShaObj.update('x-date: ' + GMTString);
-    let HMAC = ShaObj.getHMAC('B64');
-    let Authorization = 'hmac username="' + AppID + '", algorithm="hmac-sha1", headers="x-date", signature="' + HMAC + '"';
-    return { 'Authorization': Authorization, 'X-Date': GMTString }; 
+//*params fetcher is an Object
+//*return an Object with basic fetch processor & setter
+function fetcherGenerator( fetcher = {} ){
+    if( typeof fetcher !== 'object' )fetcher = {};
+    fetcher.url = '';
+    fetcher.header = '';
+    fetcher._shift = Array.prototype.shift;
+    fetcher._dataFn = function( originData ){
+        return originData;
+    };
+
+    fetcher.getAPIData = function(){
+        let afterAction = this._shift.call( arguments );
+        let args = arguments;
+        let fn = this._dataFn;
+
+        fetch( this.url, this.header )
+        .then( res => res.json() )
+        .then( data => {
+            console.log(data);
+            let formatData = fn( data );
+            afterAction( ...args, formatData );
+        })
+        .catch( errMsg => console.log(errMsg) );
+    };
+    fetcher.setHeader = function(
+        AppID = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF', 
+        AppKey = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF'){
+        let GMTString = new Date().toGMTString();
+        let ShaObj = new jsSHA( 'SHA-1', 'TEXT' );
+        ShaObj.setHMACKey( AppKey, 'TEXT' );
+        ShaObj.update( 'x-date: ' + GMTString );
+        let HMAC = ShaObj.getHMAC( 'B64' );
+        let Authorization = 'hmac username="' + AppID + '", algorithm="hmac-sha1", headers="x-date", signature="' + HMAC + '"';
+        fetcher.header = { 'Authorization' : Authorization, 'X-Date' : GMTString }; 
+    };
+    fetcher.setUrl = function( fn ){
+        fetcher.url = fn(...arguments);
+    };
+    fetcher.setDataMiddleHandler = function( fn ){
+        fetcher._dataFn = fn;
+    };
+
+    return fetcher;
 }
-let fetcher = function(THEME,CATEGORY,BASEURL){
-    let theme=THEME || 'Tourism' ,category=CATEGORY || 'Activity' ,otherParameters='';
-    let $top = 10,$skip = 0;
-    let $select='',$filter = '',$orderby='',$spatialFilter = '';
-    let $format = 'JSON';
-    let query='$top=30';
-    let base= BASEURL || 'https://ptx.transportdata.tw/MOTC/v2/';
-    let url=`${base}${theme}/${category}${otherParameters?'/' + otherParameters:''}?${query}`;
-    // let isLoaded=false;
-    function queryParamCombine(setting){
-        // console.group('query');
-        // console.log(setting);
-        let keys = Object.keys(setting);
-        let value = Object.values(setting);
-        let combination = value.reduce((acc,curr,index)=>{
-            return acc + (curr ? keys[index] + '=' + curr + '&' : '');
-        },'')
-        // console.log('query:' + combination);
-        // console.groupEnd('query');
-        query = combination;
-    };
-    function urlUpdate(){
-        url=`${base}${theme}/${category}${otherParameters?'/' + otherParameters:''}?${query}`;
-        // console.group('fetchUrl');
-        // console.log(url);
-        // console.groupEnd('fetchUrl');
-    };
-    return {
-        getHeader:function(){
-            return {headers: getAuthorizationHeader(),method:'GET'};
-        },
-        getUrl:function(){
-            queryParamCombine({$top,$skip,$select,$filter,$orderby,$spatialFilter,$format});
-            urlUpdate();
-            return url;
-        },
-        getCategory:function(){
-            return category;
-        },
-        setQuery:function(setting){
-            $top=setting.top || $top;
-            $skip=setting.skip || $skip;
-            $select=setting.select || $select;
-            $format=setting.format || $format;
-            $filter=setting.filter || $filter;
-        },
-        setSearch:function(searchString){
-            if(!searchString) return;
-            $filter=`contain(Name,${searchString})`;
-        },
-        setOtherParam:function(otherParam){
-            otherParameters = otherParam || otherParameters;
-        },
-        scopeSearch:function(lat,lon,raduis){
-            if(!lat || !lon || !raduis)return;
-            $spatialFilter=`nearby(${lat},${lon},${raduis})`;
-        },
-        getData:function(triggerFn,triggerKey,triggerMsg){
-            fetch(this.getUrl(),this.getHeader())
-            .then(res=>res.json())
-            .then(function(data){
-                console.log(data);
-                triggerFn(triggerKey, data, triggerMsg);
-            }).catch(function(errMsg){console.log(errMsg);})
+//API factory product
+let factoryStrategies = {
+    PTXData:function( originObj, [THEME, CATEGORY, BASEURL] ){
+        //mount object
+        let mountObj = originObj;
+        //Url type
+        let theme=THEME || 'Tourism' ,category=CATEGORY || 'Activity' ,otherParameters='';
+        //Query
+        let $top = 50,$skip = 0;
+        let $select='',$filter = '',$orderby='',$spatialFilter = '';
+        let $format = 'JSON';
+
+        let base= BASEURL || 'https://ptx.transportdata.tw/MOTC/v2/';
+        //Auth key
+        let AppID = 'eb78cef36689451a8aab0db192c90462';
+        let AppKey = 'BcSgzENVt1N5FxsK0SOjFiAU3eA';
+        //initial setting
+        mountObj.setUrl( paramsCombineToUrl );
+        mountObj.setHeader( AppID, AppKey );
+        mountObj.setDataMiddleHandler( APIDataFormatToFrontEndData );
+
+        //custom methods
+        function ParamsCombineToQuery( setting ){
+            let combination = '';
+            for (const key in setting) {
+                combination += setting[key] ? key + '=' + setting[key] + '&' : '';
+            }
+            return combination;
         }
+        function paramsCombineToUrl(){
+            let OP = otherParameters ? '/' + otherParameters : '';
+            let combination = base + theme + '/' + category + OP + 
+                '?' + ParamsCombineToQuery( {$top, $skip, $select, $filter, $orderby, $spatialFilter, $format} );
+            return combination;
+        }
+        function APIDataFormatToFrontEndData( originData ){
+            function truthyPushArray( dataArr ){
+                let truthyArr = [];
+                dataArr.forEach(el=>{
+                    if(el!==undefined)truthyArr.push(el);
+                });
+                return truthyArr;
+            }
+            let mapObj = originData.map(item=>{
+                return {
+                    ID: item?.ActivityID || item?.RestaurantID || item?.ScenicSpotID,
+                    Name: item?.ActivityName || item?.RestaurantName || item?.ScenicSpotName || '',
+                    Address: item?.Address || item?.City,
+                    Description: item?.Description || '',
+                    Organizer: item?.Organizer || '',
+                    Phone: String( item.Phone ).split( '、' )[0] || '',
+                    OpenTime: item?.OpenTime || String( item.StartTime ).split('T')[0] + '~' + String( item.EndTime ).split('T')[0],
+                    ClassTags: truthyPushArray( [item?.Class1,item?.Class2,item?.Class3] ),
+                    PictureUrl: truthyPushArray( [item.Picture?.PictureUrl1, item.Picture?.PictureUrl2, item.Picture?.PictureUrl3] ) || '',
+                    PictureDescri: truthyPushArray( [item.Picture?.PictureDescription1, item.Picture?.PictureDescription2, item.Picture?.PictureDescription3] ) || ''
+                }
+            })
+            return mapObj;
+        }
+
+        //mount to API
+        mountObj._dataFn = APIDataFormatToFrontEndData;
+        mountObj.getCategory = function(){
+            return category;
+        }
+        mountObj.setUrlType = function( {THEME, CATEGORY} = setting ){
+            theme = THEME || theme;
+            category = CATEGORY || category;
+            mountObj.setUrl( paramsCombineToUrl );
+        }
+        mountObj.setQuery = function( {top, skip, select, format, filter} = setting ){
+            $top = top || $top;
+            $skip = skip || $skip;
+            $select = select || $select;
+            $format = format || $format;
+            $filter = filter || $filter;
+            mountObj.setUrl( paramsCombineToUrl );
+        }
+
+        return mountObj;
     }
+}
+//*params apiName is a string to assign product
+//*return an Object with custom processor & setter
+let APIFactory = function(){
+    let apiName = Array.prototype.shift.call(arguments),
+        args = arguments,
+        basicFetcher = new fetcherGenerator();
+    let customFetcher = factoryStrategies[apiName]( basicFetcher, args );
+    return Object.create( customFetcher );
 }
--- subscribe-demo/js/eventChannel.js
+++ subscribe-demo/js/eventChannel.js
@@ -1,36 +1,84 @@
 ;
-let eventChannel = {
-    clientList: {},
-    listen: function(key, fn){
-        if(!this.clientList[key]) this.clientList[key] = [];
-        this.clientList[key].push(fn);
-    },
-    trigger: function(){
-        let key = Array.prototype.shift.call(arguments);
-        console.log(this);
-        let fnPool = this.clientList[key];
-        let foo;
-        console.log(foo = fnPool[0]);
-        for(let i=0, fn; fn=fnPool[i++];){
-            fn.apply(this, arguments);//arguments裡面的key已被抽走
-        }
-    },
-    removeListen: function(key, removeFn){
-        let fnPool = this.clientList[key];
-        if(fnPool.length === 0 || !fnPool) return false;
-        if(!removeFn){ fnPool && (fnPool.length = 0)}//removeFn為falsy後，且fnPool為truthy，才把fnPool清空
-        else{
-            for(let i=0, fn;fn = fnPool[i]; i++){
-                if(fn === removeFn){
-                    fnPool.splice(i, 1);
+let eventChannel = (function(){
+    // '_' mean origin thing
+    const _defaultName = 'default';
+    let Event = function(){
+        const _shift = Array.prototype.shift,
+            _unshift = Array.prototype.unshift;
+        let namespaceCache = {};
+        
+        const _listen = function(key, fn, cache){
+            if(!cache[key]) cache[key] = [];
+            cache[key].push(fn);
+        };
+        let _trigger = function(){
+            let self = this;
+            let cache = _shift.call(arguments);
+            let key = _shift.call(arguments);
+            let args = arguments;
+            let fnPool = cache[key];
+            if(!fnPool || !fnPool.length) return;
+
+            return fnPool.forEach(function (fn){
+                fn.apply(self, args);
+            })
+        };
+        const _removeListen = function(key, fn, cache){
+            if(!cache[key]) return;
+            if(fn){
+                for(let i = cache[key].length;i >= 0; i--){
+                    if(cache[key] === fn){ cache[key].splice(i, 1);}
                 }
             }
+            else{cache[key] = []};
         }
-    }
-}
 
-let installEvent = function (obj){
-    for(let item in eventChannel){
-        obj[item] = eventChannel[item];
-    }
-}
+        const _channel = function(channelName){
+            let namespace = channelName || _defaultName;
+            let cache = {},
+                offlineStack = [],
+                ret = {
+                    listen: function(key, fn, last){
+                        _listen(key, fn, cache);
+                        if(offlineStack === null) return;
+                        if(last === 'last'){offlineStack.length && offlineStack.pop()();}
+                        else{offlineStack.forEach(function(fn){
+                            fn();
+                        })}
+                        offlineStack = null;
+                    },
+                    trigger: function(){
+                        let fn, args, self = this;
+                        _unshift.call(arguments, cache);
+                        args = arguments;
+                        fn = function(){
+                            return _trigger.apply(self, args);
+                        };
+                        if(offlineStack)return offlineStack.push(fn);
+                        return fn();
+                    },
+                    removeListen: function(key, fn){
+                        _removeListen(key, cache, fn);
+                    }
+                }
+            return namespace ? 
+            (namespaceCache[namespace] ? namespaceCache[namespace] : namespaceCache[namespace] = ret) : ret;
+        }
+        return {
+            channel: _channel,
+            listen: function(key, fn, last){
+                let event = this.channel();
+                event.listen(key, fn, last);
+            },
+            trigger: function(){
+                let event = this.channel();
+                event.trigger.apply(this, arguments);
+            },
+            removeListen: function(key, fn){
+                let event = this.channel();
+                event.removeListen(key, fn);
+            }
+        }
+    }()
+    return Event;
+})();
